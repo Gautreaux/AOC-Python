ca6a7e45b6cafa58fd34dbfca84b251b85b91585
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2019-12-01 22:58:58 -0600

    2019 d1 part 2 complete

--- Solutions2019/y2019d1.py
+++ Solutions2019/y2019d1.py
@@ -6,10 +6,22 @@ def y2019d1(inputPath = None):
     print("2019 day 1:")
 
     newFuelCounter = 0
+    newTotalFuelCounter = 0 #for part 2
     with open(inputPath) as f:
         for line in f:
             newFuelCounter += math.floor(int(line)/3)-2
 
-    print("The total new fuel needed (part 1) is: " + str(newFuelCounter))
+            tempCtr = int(line)
 
+            while(tempCtr > 0):
+                #print(tempCtr)
+                newTemp = math.floor(tempCtr/3)-2
+                if(newTemp > 0):
+                    newTotalFuelCounter += newTemp
+                    tempCtr = newTemp
+                else:
+                    tempCtr = 0
+
+    print("The new fuel needed (part 1) is: " + str(newFuelCounter))
+    print("The total new fuel needed (part 2) is: " + str(newTotalFuelCounter))
     print("===========")
