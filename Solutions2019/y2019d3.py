e14bd808937989d27d6fdb17221ab3fb1d76d933
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2019-12-02 23:58:03 -0600

    2019 d3 pt 1 & 2

---
+++ Solutions2019/y2019d3.py
@@ -0,0 +1,167 @@
+
+def isVertical(segment):
+    return segment.start[0] == segment.end[0]
+
+def isHorizontal(segment):
+    return segment.start[1] == segment.end[1]
+
+def getSegmentCollision(Seg1, Seg2):
+    if(isVertical(Seg1) and isHorizontal(Seg2)):
+        #calculate the intersection
+        if(Seg2.start[0] <= Seg1.start[0] and Seg2.end[0] >= Seg1.start[0]) and (Seg1.start[1] >= Seg2.start[1] and Seg1.end[1] <= Seg2.start[1]):
+            return (Seg1.start[0], Seg2.start[1])
+    elif(isVertical(Seg1) and isVertical(Seg2)):
+        #calculate the intersection
+        if(Seg1.start[0] == Seg2.start[0]):
+            #share the x coordinate
+            print("You lazy boi")
+    elif(isHorizontal(Seg1) and isHorizontal(Seg2)):
+        if(Seg1.start[1] == Seg2.start[1]):
+            #share the y coordinate
+            print("You lazy mf")
+    elif(isHorizontal(Seg1) and isVertical(Seg2)):
+        return getSegmentCollision(Seg2, Seg1)
+    else:
+        print("Found one or more non-proper segments")
+    return None
+    
+
+class Segment:
+    def __init__(self, startPair, endPair):
+        #can assume the the start is above/left of the ending point
+        if(startPair[0] > endPair[0]):
+            self.start = endPair
+            self.end = startPair
+        elif(endPair[1] > startPair[1]):
+            self.start = endPair
+            self.end = startPair
+        else:
+            self.start = startPair
+            self.end = endPair
+        self.actualStart = startPair
+    #TODO - overload getting by []
+
+def manhattanDistance(pair1, pair2):
+    return abs(pair1[0]-pair2[0]) + abs(pair1[1]-pair2[1])
+
+def y2019d3(inputPath = None):
+    if(inputPath == None):
+        inputPath = "Input2019/d3.txt"
+    print("2019 day 3:")
+
+    with open(inputPath) as f:
+        myStr = f.readline()
+        myStr = myStr.strip()
+        myStr += ", "
+        #split on commas
+        wirePath = []
+        while(myStr.find(",") > 0):
+            commaIndex = myStr.find(",")
+            wirePath.append(myStr[0:commaIndex])
+            myStr = myStr[commaIndex+1:]
+        
+        segmentList = []
+
+        #now process
+        nowX = 0
+        nowY = 0
+        for movement in wirePath:
+            instr = movement[0:1]
+            amount = int(movement[1:])
+
+            start = (nowX, nowY)
+
+            if(instr == 'R'):
+                nowX += amount
+            elif(instr == 'L'):
+                nowX -= amount
+            elif(instr == 'U'):
+                nowY += amount
+            else:
+                nowY -= amount
+            
+            end = (nowX, nowY)
+            segmentList.append(Segment(start, end))
+    
+        myStr2 = f.readline()
+        myStr2 = myStr2.strip()
+        myStr2 += ", "
+        
+        #now parse the second wire
+        wirePath = []
+        while(myStr2.find(",") > 0):
+            commaIndex = myStr2.find(",")
+            wirePath.append(myStr2[0:commaIndex])
+            myStr2 = myStr2[commaIndex+1:]
+
+        #and process
+        nowX = 0
+        nowY = 0
+        closestIntersection = float("inf")
+        closestIntersectionLocation = None
+
+        #part 2
+        closestStepIntersection = float("inf")
+        netPathDistance = 0 #total distance by wire two
+
+        for movement in wirePath:
+            instr = movement[0:1]
+            amount = int(movement[1:])
+
+            start = (nowX, nowY)
+
+            if(instr == 'R'):
+                nowX += amount
+            elif(instr == 'L'):
+                nowX -= amount
+            elif(instr == 'U'):
+                nowY += amount
+            else:
+                nowY -= amount
+            
+            end = (nowX, nowY)
+
+            sTemp = Segment(start, end)
+
+            for segment in segmentList:
+                res = getSegmentCollision(segment, sTemp)
+                if(res != None):
+                    distance = manhattanDistance(res, (0,0))
+                    if(distance == 0):
+                        continue
+                    elif distance < closestIntersection:
+                        closestIntersection = distance
+                        closestIntersectionLocation = res
+
+            #part 2 loop
+            pathLength = 0
+            for segment in segmentList:
+                res = getSegmentCollision(segment, sTemp)
+                if(res != None):
+                    if(res[0] == 0 and res[1] == 0):
+                        pathLength += manhattanDistance(segment.start, segment.end)
+                        continue
+                    
+                    thisDistance = manhattanDistance(start, res) + netPathDistance #for wire two
+                    thisDistance += manhattanDistance(segment.actualStart, res) + pathLength #for wire one
+                
+                    if(thisDistance < closestStepIntersection):
+                        closestStepIntersection = thisDistance
+                        #break #cant do better than this
+
+                pathLength += manhattanDistance(segment.start, segment.end)
+
+            netPathDistance += amount
+    
+    print("The closest intersection (part 1) is at distance "+ str(closestIntersection))
+    print("The closest path intersection (part 2) is at distance "+ str(closestStepIntersection))
+
+
+
+
+        
+    
+    print("===========")
+
+
+#84 is not correct
--- masterFile.py
+++ masterFile.py
@@ -1,5 +1,5 @@
 #the main file for the solution
 
 if __name__ == "__main__":
-    from Solutions2019.y2019d2 import y2019d2
-    y2019d2()
+    from Solutions2019.y2019d3 import y2019d3
+    y2019d3()
