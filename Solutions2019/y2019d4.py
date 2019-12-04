ac3ee32bcd675b180229c943b3d327e2bc96b8ab
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2019-12-03 23:07:16 -0600

    2019 d4 p1

--- Solutions2019/y2019d4.py
+++ Solutions2019/y2019d4.py
@@ -1,12 +1,44 @@
-from AOC_Lib.about import printConnectionMessage as pcm
 
-def y2019d4(inputPath = None):
-    if(inputPath == None):
-        inputPath = "Input2019/d4.txt"
+def hasTwoAdjacent(intIn):
+    i = str(intIn)
+
+    for ii in range(0,len(i)-1):
+        if(i[ii] == i[ii+1]):
+            return True
+    return False
+
+def isIncreasing(intIn):
+    i = str(intIn)
+
+    for ii in range(0,len(i)-1):
+        if(i[ii] > i[ii+1]):
+            return False
+    return True
+
+def isValidValue(intIn):
+    if(len(str(intIn)) != 6):
+        return False
+    #no need to check range
+    if(not(hasTwoAdjacent(intIn))):
+        return False
+    return isIncreasing(intIn)
+
+def y2019d4(inputStr = None):
+    if(inputStr == None):
+        inputStr = "272091-815432"
     print("2019 day 4:")
-    pcm()
 
-    with open(inputPath) as f:
-        for line in f:
-            pass
+    inputStr = inputStr.strip()
+    dash = inputStr.find("-")
+    lb = int(inputStr[0:dash])
+    ub = int(inputStr[dash+1:])
+
+    i = lb
+    validCount = 0
+    while(i <= ub):
+        if(isValidValue(i)):
+            validCount+=1
+        i+=1
+
+    print("The valid count (part 1) is " + str(validCount))
     print("===========")
