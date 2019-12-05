34f55412e29342d60f3935faea735466c69aa63f
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2019-12-04 23:00:05 -0600

    lib setup

--- AOC_Lib/point.py
+++ AOC_Lib/point.py
@@ -2,9 +2,52 @@
 
 
 class point2():
-    def __init__(self):
-        pass
+    def __init__(self, p1, p2 = None):
+        try:
+            if(len(p1) == 1):
+                raise TypeError #a dummy error
+            if(len(p1) == 2):
+                self.x = p1[0]
+                self.y = p1[1]
+        except TypeError:
+            self.x = p1
+            self.y = p2
 
+        #TODO - validate that p1 and p2 are both numeric types
+
+    def __len__(self):
+        return 2
+    
+    def __getitem__(self, key):
+        if(key == 0 or key == 'x'):
+            return self.x
+        elif(key == 1 or key == 'y'):
+            return self.y
+        
+        raise KeyError("Invalid key ("+str(key)") in point2")
+
+    def __setitem__(self, key, item):
+        #TODO - validate item on success
+        if(key == 0 or key == 'x'):
+            self.x = item
+        elif(key == 0 or key == 'y'):
+            self.y = item
+        
+        raise KeyError("Invalid key ("+str(key)") in point2"))
+
+    def __repr__(self):
+        return str(self)
+    
+    def __str__(self):
+        return "("+str(self.x)+", "+str(self.y)+")"
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    #TODO - equality and addition/subtraction
+
+
+#TODO - finish point3 implementation
 class point3():
     def __init__(self):
         pass
---
+++ Solutions2019/y2019d5.py
@@ -0,0 +1,12 @@
+
+
+def y2019d5(inputPath = None):
+    if(inputPath == None):
+        inputPath = "Input2019/d5.txt"
+    print("2019 day 5:")
+
+    with open(inputPath) as f:
+        for line in f:
+            pass
+
+    print("===========")
