9ecd5cddcb244366d904d8e49fb04cf3b178509c
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2019-12-08 23:12:12 -0600

    Update masterFile.py

--- masterFile.py
+++ masterFile.py
@@ -1,5 +1,5 @@
 #the main file for the solution
 
 if __name__ == "__main__":
-    from Solutions2019.y2019d8 import y2019d8
-    y2019d8()
+    from Solutions2019.y2019d7 import y2019d7
+    y2019d7()
