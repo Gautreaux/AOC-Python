cbbab992e585b64d886244e100169338ec1aa796
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2019-12-10 00:10:01 -0600

    boutta refactor (agressively)

--- Solutions2019/y2019d10.py
+++ Solutions2019/y2019d10.py
@@ -4,57 +4,23 @@ import math
 def dist(p1, p2):
     return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
 
-def getAsteroidsOnRay(sightRay, pointXY, worldMap):
-    width = len(worldMap[0])
-    height = len(worldMap)
-
-    def getYFromX(x):
-        try:
-            return (sightRay[1]/sightRay[0])*(x-pointXY[0])+pointXY[1]
-        except ZeroDivisionError:
-            return float('inf')
-
-    yReturn = []
-    for x in range(width):
-        yTemp = getYFromX(x)
-        #TODO - can do this check with an exception and catch
-
-
-        if(yTemp != float('inf')):
-            try:
-                if(int(yTemp) != yTemp):
-                    raise TypeError
-                yTemp = int(yTemp)
-                if(worldMap[yTemp][x] == '#'):
-                    #add this asteroid to the list
-                    yReturn.append((x,yTemp))
-            except TypeError:
-                pass
-            except IndexError:
-                pass
-        else:
-            #y is a vertical line
-            for y in range(height):
-                yReturn.append((x,y))
-    return yReturn
+def getRay(p1, p2):
+    return ((p2[0]-p1[0]),(p2[1]-p1[1]))
 
 
 def canSee(stationXY, asteroidXY, worldMap):
     #the vector from the station to the asteroid
-    sightRay = (asteroidXY[0]-stationXY[0], asteroidXY[1]-stationXY[1])
-
-    w = getAsteroidsOnRay(sightRay, stationXY, worldMap)
+    sightRay = getRay(stationXY, asteroidXY)
+    minDist = dist(stationXY, asteroidXY)
     
-    minDist = float('inf')
-    minPair = None
-    for a in w:
-        d = dist(a, stationXY)
-        if d == 0:
-            continue
-        elif d < minDist:
-            minDist = d
-            minPair = a
-    return minPair == asteroidXY
+    for y in range(len(worldMap)):
+        for x in range(len(worldMap[0])):
+            if((x,y) == stationXY):
+                continue
+            if((x,y) == asteroidXY):
+                continue
+            if(dist(stationXY, (x,y)) < minDist and getRay(stationXY, (x,y)) == sightRay):
+                return False
 
 def getSightCount(lineList, xStat, yStat):
     'return the number of asteroids visible from this point'
@@ -89,6 +55,12 @@ def y2019d10(inputPath = None):
         bestXY = (None, None)
         bestCount = 0
 
+        asteroidPos = [] # a list of all the asteroids
+
+        for y in range(len(lineList)):
+            for x in range(len(lineList[0])):
+                asteroidPos.append((x,y))
+
         for y in range(len(lineList)):
             for x in range(len(lineList[0])):
                 # print(lineList[y][x], end="")
@@ -99,7 +71,7 @@ def y2019d10(inputPath = None):
                     bestCount = k
                     bestXY = (x, y)
 
-            print("Advancing to line " + str(y))
+            print("Advancing to line " + str(y+1))
             # print("")
 
         print("The best location " + str(bestXY) + " can see " + str(bestCount));
