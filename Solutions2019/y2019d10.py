530dd18a2f6743128058b2939ca7316539473080
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2019-12-10 15:30:21 -0600

    2019 d10 p2 done

--- Solutions2019/y2019d10.py
+++ Solutions2019/y2019d10.py
@@ -11,6 +11,53 @@ def getRay(p1, p2):
     rmag = getDist(r, (0,0))
     return (round(r[0]/rmag,6), round(r[1]/rmag,6))
 
+def findStandardFormAngularDistance(station, asteroid):
+    #get the angle between the station and the asteroid in radians
+    #in standard form i might add
+    ray = getRay(station, asteroid)
+    ang = math.atan2(ray[1], ray[0])
+    if(ang < 0):
+        ang += math.pi*2
+    return ang
+
+def findAngularDistance(station, laserOrientation, asteroid):
+    #laser orientation is already in [0, 2pi)
+    
+    astStandDist = findStandardFormAngularDistance(station, asteroid)
+    if(astStandDist == laserOrientation):
+        return 0
+    if(laserOrientation > astStandDist):
+        return laserOrientation - astStandDist
+    if(laserOrientation < astStandDist):
+        return 2*math.pi-astStandDist+laserOrientation
+    raise ValueError("How did you get here?")
+    
+
+def vaporize(asteroidList, laserOrientation, station):
+    #laser orientation should be in range [0,2pi)
+    #find the closest asteroid to vaporize 
+
+    minAngDist = 2*math.pi
+    linearDist = float("inf")
+    minAst = None
+
+    for asteroid in asteroidList:
+        if(asteroid == station):
+            continue
+        angDist = findAngularDistance(station, laserOrientation, asteroid)
+        linDist = getDist(station, asteroid)
+        if(angDist < minAngDist):
+            minAngDist = angDist
+            linearDist = linDist
+            minAst = asteroid
+        elif(angDist == minAngDist and linDist < linearDist):
+            linearDist = linDist
+            minAst = asteroid
+    
+    if(minAngDist < 0):
+        print("Found negative angular distance: " + str(minAngDist) + " on " + str(minAst))
+    return (minAngDist, minAst)
+
 def y2019d10(inputPath = None):
     if(inputPath == None):
         inputPath = "Input2019/d10.txt"
@@ -64,7 +111,55 @@ def y2019d10(inputPath = None):
                 bestCount = totalVisible
                 bestXY = asteroid
 
-        print("The best location " + str(bestXY) + " can see " + str(bestCount));
+        print("PART 1:The best location " + str(bestXY) + " can see " + str(bestCount));
+
+        laserOrientation = math.pi/2 #90deg in radians (up on the standard repr)
+        #rotates clockwise
+
+        asteroidPosCopy = asteroidPos #store a copy of the original asteroid list
+
+        stationLocation = bestXY
+
+        #recenter the list have station at 0,0
+        fixedAsteroidList = []
+        for asteroid in asteroidPos:
+            fixedAsteroidList.append((asteroid[0]-stationLocation[0],asteroid[1]-stationLocation[1]))
+
+        #modify so that positive y is the up direction
+        asteroidPos = []
+        for fAsteroid in fixedAsteroidList:
+            asteroidPos.append((fAsteroid[0], -fAsteroid[1]))
+
+
+        #items destroyed
+        destorycounter = 0
+
+        for i in range(1,201):
+            (aDist, asteroid) = vaporize(asteroidPos, laserOrientation, (0,0))
+
+            origAsteroid = asteroidPosCopy[asteroidPos.index(asteroid)]
+
+            if(i == 200):
+                break
+
+            # print("["+str(i)+"] " + str(asteroid) + " " + str(origAsteroid))
+            asteroidPos.remove(asteroid)
+            asteroidPosCopy.remove(origAsteroid)
+            
+            if(aDist == 0 and not(i == 1)):
+                print("Warning: no laser motion between destroys: " + str(i))
+            laserOrientation -= aDist
+            laserOrientation -= .0000001 #make move for the next one
+            if(laserOrientation < 0):
+                laserOrientation+=2*math.pi
+
+            # destorycounter+=1
+            # print("Destroyed " + str(destorycounter))
+
+            
+        # (aDist, asteroid) = vaporize(asteroidPos, laserOrientation, (0,))
+        origAsteroid = asteroidPosCopy[asteroidPos.index(asteroid)]
+        print("The 200th (part 2) asteroid to be destroyed is " + str(origAsteroid) + ". part2 value: " + str(origAsteroid[0]*100+origAsteroid[1]))
 
     print("===========")
 
@@ -72,3 +167,7 @@ def y2019d10(inputPath = None):
     #728 too high
     #404 too high
     #295 incorrect
+
+    #part 2
+    #1413 too high
+    #1213 too high
--- masterFile.py
+++ masterFile.py
@@ -3,3 +3,4 @@
 if __name__ == "__main__":
     from Solutions2019.y2019d10 import y2019d10
     y2019d10()
+    # y2019d10("Input2019/d10-sample.txt")
