eee4544aaaa16a91fad08b349b1edb2c6f693529
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2019-12-12 23:37:59 -0600

    playable brick breaker

--- Solutions2019/y2019d13.py
+++ Solutions2019/y2019d13.py
@@ -1,7 +1,10 @@
 import math
+import random
 
 from Solutions2019.y2019d5 import y2019d5
 
+def get2dHash(pos):
+    return hash(pos[0]*10000+pos[1])
 
 class GameState():
     def __init__(self):
@@ -9,13 +12,41 @@ class GameState():
         self.blockCount = 0
         self.xValue = None
         self.yValue = None
+        self.score = None
 
+        self.memMap = {}
+
+        self.minX = 0
+        self.minY = 0
+        self.maxX = 0
+        self.maxY = 0
+
+        self.charmap = {0:" ", 1:"█", 2:"B", 3:"_", 4:"."}
 
     def __del__(self):
         pass
 
     def sendCommand(self):
-        pass
+        self.draw()
+        i = input("Move: ") 
+        if(i == 'l'):
+            return -1
+        elif(i == 'r'):
+            return 1
+        elif(i == ""):
+            return 0
+        else:
+            return self.draw()
+
+    def draw(self):
+        vList = []
+        for y in range(self.minY, self.maxY+1):
+            for x in range(self.minX, self.maxX+1):
+                if(get2dHash((x,y)) in self.memMap):
+                    print(self.charmap[self.memMap[get2dHash((x,y))]], end = "")
+                else:
+                    print(self.charmap[0], end = "")
+            print("")
 
     def receiveCommand(self, c):
         if(self.xValue == None):
@@ -31,12 +62,31 @@ class GameState():
         pos = (self.xValue, self.yValue)
         self.xValue = None
         self.yValue = None
-        if(tileID == 2):
-            if pos not in self.blockList:
-                self.blockList.append(pos)
+        if(pos == (-1, 0)):
+            self.score = tileID
+            if(len(self.blockList) == 0):
+                print("Block list empty. Score (part2): " + str(self.score))
+            return
         else:
-            if pos in self.blockList:
-                self.blockList.remove(pos)
+            if(tileID == 2):
+                if pos not in self.blockList:
+                    self.blockList.append(pos)
+            else:
+                if pos in self.blockList:
+                    self.blockList.remove(pos)
+
+        if(pos[0] < self.minX):
+            self.minX = pos[0]
+        if(pos[0] > self.maxX):
+            self.maxX = pos[0]
+        if(pos[1] < self.minY):
+            self.minY = pos[1]
+        if(pos[1] > self.maxY):
+            self.maxY = pos[1]
+
+        self.memMap[get2dHash(pos)] = tileID
+
+
 
 
 def y2019d13(inputPath = None):
