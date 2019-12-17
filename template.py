05103d909a7cfe04848e34b1fbbceda1d2b83e84
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2019-12-17 14:09:24 -0600

    Create template.py

---
+++ template.py
@@ -0,0 +1,11 @@
+
+def y2019d99(inputPath = None):
+    if(inputPath == None):
+        inputPath = "Input2019/d99.txt"
+    print("2019 day 99:")
+
+    with open(inputPath) as f:
+        for line in f:
+            print(line)
+        
+    print("===========")
