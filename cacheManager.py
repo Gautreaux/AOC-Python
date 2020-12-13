da7e1f3f4bc75a001773795f7c58c99d0e9b8996
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2020-12-12 22:06:14 -0600

    Finalized code for list all required files

--- cacheManager.py
+++ cacheManager.py
@@ -1,35 +1,54 @@
 
 
+from Util.Util import isValidDateCode, splitDateCode
+import modulefinder
+from modulefinder import Module, ModuleFinder
+from importlib import import_module
 from Util.FileUtil import getAllSubDirectories  
 from typing import List
-from modulefinder import Module, ModuleFinder
-from os import getcwd
 
+from os import getcwd, path
 
-def getAllModulesInDay(dateCode : str) -> List[Module]:
+class ModuleFinderException(Exception):
+    pass
+
+def getAllPyFilesForDay(dateCode : str) -> List[str]:
+    '''Get a list of all local files required for running a particular datecode'''
     
+    # TODO - Testing (some problem exists somewhere)
+    # This should work once https://bugs.python.org/issue40350 fixed?
+    #   "import math" seems to break things?
+    #solutionPath = "Solutions2019/y2019d11.py"    
     # TODO - resolve datecode to file path
+    assert(isValidDateCode(dateCode))
+    y,d = splitDateCode(dateCode)
+
+    solutionPath = f"Solutions{y}/y{y}d{d}.py"
     
-
-    itemPath = "Solutions2020/y2020d11.py"
-    # itemPath = "Solutions2019/y2019d11.py"
-
     subDirs = getAllSubDirectories(getcwd())
-    print(subDirs)
-
+    
     finder = ModuleFinder(subDirs)
-    finder.run_script(itemPath)
+    try:
+        finder.run_script(solutionPath)
+    except FileNotFoundError:
+        raise
+    except:
+        # it seems to usually work, but in case no, want to log
+        print(f"Exception Occurred when running module finder for {dateCode}")
+        raise ModuleFinderException
 
-    mm: Module = None
+    files = []
+
     for name, mod in finder.modules.items():
-        print(f"{mod} {mod.__name__} {mod.__path__}")
-        mm = mod
-        
-    print(dir(mm))
-        
-    finder.report()
+        # print(f"{mod} {mod.__name__} {mod.__path__}")
+
+        if mod.__path__ is None:
+            print(dir(mod))
+            files.append(mod.__file__)
+
+    return files
 
 
 
 if __name__ == "__main__":
-    getAllModulesInDay("apple")
+    print(getAllPyFilesForDay("y2020d25"))
