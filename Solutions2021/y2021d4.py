1c3dd6f8d59fe7ef0889c09660e13540f37cd639
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2021-12-06 23:58:17 -0600

    2021 d4 d7 done

--- Solutions2021/y2021d4.py
+++ Solutions2021/y2021d4.py
@@ -1,7 +1,7 @@
 # from AOC_Lib.name import *
 
 from copy import deepcopy
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Optional, Tuple
 
 NEGATIVE_ZERO = -0xFFFFFFFF
 
@@ -11,105 +11,119 @@ class BreakAll(Exception):
 class BingoBoard:
     def __init__(self, num_rows:int=0, num_cols:int=0) -> None:
         self._dims = (num_rows, num_cols)
-        self._board = []
+        self._board = [None]*(num_rows*num_cols)
+        self.clearMarks()
 
-        a_row = [None]*num_cols
-
-        while len(self._board) < num_rows:
-            self._board.append(deepcopy(a_row))
-
-    def markNumber(self, num: int) -> Tuple[bool, bool]:
+    def markCell(self, cell:int, check:Optional[int]=None) -> None:
         """
-        Returns pair (was_marked, marked_winner)
-            bool if any number was marked
-            bool if any marked number is on a winning part of the board
-
-        NOTE: marked_winner may return false even though the board
-            was already a winner. 
-        NOTE: marked_winner may return false even `was_marked`
-            is true AND the board was a winner
+            Mark the cell as taken,
+            if check is provided, assert the value at the cell matches
         """
 
-        marked_something = False
-        is_now_winner = False
+        if check != None:
+            assert(self._board[cell] == check)
+        self._marked[cell] = True
 
-        for i, row in enumerate(self._board):
-            for j, cell in enumerate(row):
-                if cell != num:
-                    continue
+    def setCell(self, cell:int, value: int, allow_overwrite:bool = False, clear_marked:bool = True) -> None:
+        """
+            Set the cell to value
+            error if allow_overwrite is false and value was set
+        """
 
-                if cell == 0:
-                    self._board[i][j] == NEGATIVE_ZERO
-                else:
-                    self._board[i][j] == -cell
+        if not allow_overwrite:
+            assert(self._board[cell] == None)
+        self._board[cell] = value
 
-                marked_something = True
-
-                if is_now_winner:
-                    # someone else already marked this as winner
-                    # so no need to check anymore
-                    continue
-
-                # check and see if this is now a winner
-                
-                # check the row
-                row_winner = True
-                for j_t in range(self._dims[1]):
-                    if self._board[i][j_t] >= 0:
-                        row_winner = False
-                        break
-                if row_winner:
-                    is_now_winner = True
-                    continue
-
-                # check the column
-                col_winner = True
-                for i_t in range(self._dims[0]):
-                    if self._board[i_t][j] >= 0:
-                        col_winner = False
-                        break
-                if col_winner:
-                    is_now_winner = True
-                    continue
-
-                # check the diagonal
-                # assert(self._dims[0] == self._dims[1])
-                # if(i == j):
-                # need to check the \ diagonal
-                # also need to check the / diagonal
-                raise NotImplementedError()
-
-        return (marked_something, is_now_winner)
+        if clear_marked:
+            self._marked[cell] = False
     
-    def isWinner(self) -> bool:
-        raise NotImplementedError()
+    def isColWinner(self, column_id: int) -> bool:
+        assert(column_id >= 0 and column_id < self._dims[1])
+        for i in range(self._dims[0]):
+            if not self._marked[i*self._dims[1] + column_id]:
+                return False
+        return True
+
+    def isRowWinner(self, row_id: int) -> bool:
+        assert(row_id >= 0 and row_id < self._dims[0])
+        for i in range(self._dims[1]):
+            if not self._marked[row_id*self._dims[1] + i]:
+                return False
+        return True
+
+    def isDiagonalWinner(self) -> bool:
+        """
+            Check if the \ diagonal is winner
+            Only works on squares
+        """
+        assert(self._dims[0] == self._dims[1])
+        for i in range(self._dims[0]):
+            if not self._marked[i*self._dims[1] + i]:
+                return False
+        return True
+
+    def isDiagonalWinnerAlt(self) -> bool:
+        """
+            Check if the / diagonal is winner
+            Only works on squares
+        """
+        assert(self._dims[0] == self._dims[1])
+        for i in range(self._dims[0]):
+            if not self._marked[i*self._dims[1] + self._dims[1] - i - 1]:
+                return False
+        return True
+
+    def isWinner(self, print_winner=True) -> bool:
+        """Check all rows, columns, and diagonals for winners"""
+        for r in range(self._dims[0]):
+            if self.isRowWinner(r):
+                if print_winner:
+                    print(f"Winner in row {r}")
+                return True
+        for c in range(self._dims[1]):
+            if self.isColWinner(c):
+                if print_winner:
+                    print(f"Winner in col {c}")
+                return True
+        # diagonals dont count
+        # if self.isDiagonalWinner() or self.isDiagonalWinnerAlt():
+        #     return True
+        return False
 
     def getUnmarkedSum(self) -> int:
-        s = 0
-        for i, row in enumerate(self._board):
-            for j, cell in enumerate(row):
-                if cell >= 0:
-                    s += cell
-        return s
+        total = 0
+        for c,m in zip(self._board, self._marked):
+            if m is False:
+                total += c
+        return total
 
     @classmethod
     def fromLineIter(cls, itr: Iterable, num_rows:int = 5) -> "BingoBoard":
-        lines = []
+        cells = []
+
+        num_cols = None
 
         for _ in range(num_rows):
             l = next(itr)
-            lines.append(l.replace("  ", " "))
+            cells.extend(map(int, l.replace("  ", " ").split(" ")))
 
-        num_cols = len(lines[0].split(" "))
+            if num_cols == None:
+                num_cols = len(cells)
+            else:
+                # check that the latest line did append the proper number
+                assert(len(cells) % num_cols == 0)
 
         b = BingoBoard(num_rows=num_rows, num_cols=num_cols)
-
-        for i, row_s in enumerate(lines):
-            row = row_s.split(" ")
-            for j, cell_s in enumerate(row):
-                assert(int(cell_s) >= 0)
-                b._board[i][j] = int(cell_s)
+        b._board = cells
         return b
+
+    def markByNumber(self, value):
+        for i,k in enumerate(self._board):
+            if k == value:
+                self.markCell(i)
+
+    def clearMarks(self):
+        self._marked = [False]*(self._dims[0]*self._dims[1])
 
 
 def y2021d4(inputPath = None):
@@ -142,16 +156,50 @@ def y2021d4(inputPath = None):
             break
 
     print(f"Loaded {len(bingo_boards)} bingo boards, {len(number_list)} numbers")
+    print(number_list)
 
     try:
         for number in number_list:
             for b in bingo_boards:
-                _,w = b.markNumber(number)
-                if w:
+                b.markByNumber(number)
+                if b.isWinner():
                     Part_1_Answer = b.getUnmarkedSum() * number
+                    print(f"Found first winner after number {number}, um_sum {b.getUnmarkedSum()}")
+                    print(list(zip(b._board, b._marked)))
                     raise BreakAll
         raise RuntimeError("No winning board found")
     except BreakAll:
         pass
+
+    print(bingo_boards[-1]._board)
+
+    for b in bingo_boards:
+        b.clearMarks()
+    o_non_winners = bingo_boards
+
+    for number in number_list:
+        non_winners = []
+        this_winners = []
+
+        for b in o_non_winners:
+            b.markByNumber(number)
+            if b.isWinner(print_winner=False):
+                print(f"Board removed after {number} : {b._board[:5]}")
+                this_winners.append(b)
+            else:
+                non_winners.append(b)
+
+        if len(non_winners) == 0:
+            assert(len(this_winners) == 1)
+            last_winner = this_winners[0]
+            break
+        else:
+            o_non_winners = non_winners
+
+    print((last_winner.getUnmarkedSum(), number))
+    print(last_winner._board)
+    Part_2_Answer = last_winner.getUnmarkedSum() * number
+
+    assert(Part_2_Answer > 2896)
 
     return (Part_1_Answer, Part_2_Answer)
---
+++ Solutions2021/y2021d7.py
@@ -0,0 +1,53 @@
+# from AOC_Lib.name import *
+
+def y2021d7(inputPath = None):
+    if(inputPath == None):
+        inputPath = "Input2021/d7.txt"
+    print("2021 day 7:")
+
+    Part_1_Answer = None
+    Part_2_Answer = None
+    lineList = []
+
+    with open(inputPath) as f:
+        for line in f:
+            line = line.strip()
+            lineList.append(line)
+    
+    positions = list(map(int, lineList[0].split(",")))
+
+    mi = min(positions)
+    ma = max(positions)
+
+    print((mi, ma))
+
+    best_cost = None
+    best_cost_2 = None
+
+    steps_lookup = [0]
+    while len(steps_lookup) < (ma + 2):
+        steps_lookup.append(steps_lookup[-1] + len(steps_lookup))
+
+    print(steps_lookup[:6])
+
+    for i in range(mi, ma):
+        total = 0
+        total_2 = 0
+        for p in positions:
+            a = abs(i-p)
+            total += a
+            total_2 += steps_lookup[a]
+        
+        if i == 0:
+            best_cost = total
+            best_cost_2 = total_2
+        else:
+            if best_cost > total:
+                best_cost = total
+            if best_cost_2 > total_2:
+                best_cost_2 = total_2
+
+    Part_1_Answer = best_cost
+    Part_2_Answer = best_cost_2
+
+    return (Part_1_Answer, Part_2_Answer)
