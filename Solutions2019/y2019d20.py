9401bc13570ad5942e37001f75f2b88b35765b39
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2022-06-29 15:00:27 -0500

    some more done; some more started

--- Solutions2019/y2019d20.py
+++ Solutions2019/y2019d20.py
@@ -1,8 +1,12 @@
 # from AOC_Lib.name import *
 
-from collections import defaultdict, deque
+from collections import defaultdict, deque, namedtuple
 from enum import IntEnum, unique
 
+
+Pos_T = tuple[int, int]
+
+PortalConnection_T = namedtuple("PortalConnection_T", "label outside_pos inside_pos")
 
 @unique
 class TileType(IntEnum):
@@ -23,6 +27,266 @@ class TileType(IntEnum):
             return " "
 
 
+class DonutMaze:
+    
+    def __init__(self, maze:list[list[TileType]], portal_labels:dict[str, PortalConnection_T]) -> None:
+        self._maze = maze
+        self._labels_to_portals = portal_labels
+        self._positions_to_portals = self._getPositionsToPortals()
+
+    @classmethod
+    def fromLines(cls, lineList: list[str]) -> 'DonutMaze':
+        """Build an return an object from the line list"""
+        # assert that the maze "starts at 2,2"
+        assert(lineList[0][0] == " ")
+        assert(lineList[1][0] == " ")
+        assert(lineList[0][1] == " ")
+        assert(lineList[1][1] == " ")
+        assert(lineList[0][2] == " ")
+        assert(lineList[1][2] == " ")
+        assert(lineList[2][0] == " ")
+        assert(lineList[2][1] == " ")
+        assert(lineList[2][2] == "#")
+
+        my_maze: list[list[TileType]] = []
+        portal_candidates = {}
+
+        # parse the input and find walls, passages, tiles, etc
+        for y_value, row in enumerate(lineList):
+            maze_row: list[TileType] = []
+            for x_value, char in enumerate(row):
+                if char == ".":
+                    maze_row.append(TileType.PASSAGE)
+                elif char == "#":
+                    maze_row.append(TileType.WALL)
+                elif char == " ":
+                    maze_row.append(TileType.WHITESPACE)
+                elif char == "\n":
+                    break
+                elif char == "\r":
+                    break
+                elif ((ord(char) >= ord('A')) and (ord(char) <= ord('Z'))):
+                    portal_candidates[(x_value, y_value)] = char
+                    maze_row.append(TileType.WHITESPACE)
+                else:
+                    raise RuntimeError(f"Unrecognized character: {char}")
+            my_maze.append(maze_row)
+
+        # local helper function
+        def getPassagePosition(x_1,y_1,x_2, y_2) -> Pos_T:
+            """get the passage position from the two letter positions
+                For the two positions of the letters,
+                    return the position of the adjacent PASSAGE 
+            """
+            if y_1 == y_2:
+                # horizontal case
+                candidate_x_1 = min(x_1, x_2) - 1
+                candidate_x_2 = max(x_1, x_2) + 1 # candidate_1 + 2?
+                candidate_y_1 = y_1
+                candidate_y_2 = y_1
+            else:
+                # vertical case
+                candidate_x_1 = x_1
+                candidate_x_2 = x_1
+                candidate_y_1 = min(y_1, y_2) - 1
+                candidate_y_2 = max(y_1, y_2) + 1
+            
+            for x,y in [(candidate_x_1, candidate_y_1), (candidate_x_2, candidate_y_2)]:
+                try:
+                    if x < 0 or y < 0:
+                        continue
+                    # print(f"Checking {x},{y} -> ", my_maze[y][x])
+                    if my_maze[y][x] == TileType.PASSAGE:
+                        return (x,y)
+                except IndexError:
+                    pass
+            raise RuntimeError("No neighboring position was a passage")
+
+        # mapping of portal label to position
+        matched_portals = defaultdict(list)
+        
+        # now we want to find and update all passages with their tile type
+        while portal_candidates:
+                (this_x, this_y), this_letter = next(iter(portal_candidates.items()))
+
+                num_matches = 0
+
+                for other_x, other_y in [(this_x + 1, this_y), (this_x - 1, this_y), (this_x, this_y + 1), (this_x, this_y - 1)]:
+                    try:
+                        other_letter = portal_candidates[(other_x, other_y)]
+                    except KeyError:
+                        continue
+                    
+                    if other_x > this_x:
+                        portal_name = f"{this_letter}{other_letter}"
+                    elif other_x < this_x:
+                        portal_name = f"{other_letter}{this_letter}"
+                    elif other_y > this_y:
+                        portal_name = f"{this_letter}{other_letter}"
+                    else:
+                        portal_name = f"{other_letter}{this_letter}"
+                    # print("NAME: {} at {},{}".format(portal_name, this_x, this_y))
+                    portal_pos = getPassagePosition(this_x, this_y, other_x, other_y)
+                    matched_portals[portal_name].append(portal_pos)
+                    num_matches += 1
+                    my_maze[portal_pos[1]][portal_pos[0]] = TileType.PORTAL
+                    portal_candidates.pop((this_x, this_y))
+                    portal_candidates.pop((other_x, other_y))
+                if num_matches == 0:
+                    raise RuntimeError(f"This {(this_x, this_y)} does not have another candidate nearby")
+                if num_matches > 1:
+                    raise RuntimeError(f"This {(this_x, this_y)} does not multiple candidate nearby")
+        
+        # check that all the portals have exactly two candidates in their named pair
+        for k,v in matched_portals.items():
+            if k in ['ZZ', 'AA']:
+                assert(len(v) == 1)
+                continue
+            if len(v) != 2:
+                print(f"Key {k} got only portal {v} ")
+            assert(len(v) == 2)
+
+        fixed_portals = {}
+
+        maze_w = len(my_maze[0])
+        maze_h = len(my_maze)
+        for label, positions in matched_portals.items():
+            first_pos = positions[0]
+            first_pos_is_outside = False
+
+            does_x_imply_outside = first_pos[0] < 4 or (first_pos[0] + 4) >= maze_w
+            does_y_imply_outside = first_pos[1] < 4 or (first_pos[1] + 4) >= maze_h
+
+            first_pos_is_outside =  does_x_imply_outside or does_y_imply_outside
+
+            if len(positions) == 1:
+                assert(label in ['AA', 'ZZ'])
+                assert(first_pos_is_outside)
+                positions.append(None)
+            else:
+                assert(len(positions) == 2)
+            
+            if first_pos_is_outside:
+                f_portal = PortalConnection_T(
+                    label=label,
+                    outside_pos=positions[0],
+                    inside_pos=positions[1],
+                )
+            else:
+                f_portal = PortalConnection_T(
+                    label=label,
+                    outside_pos=positions[1],
+                    inside_pos=positions[0],
+                )
+
+            fixed_portals[label] = f_portal
+
+        s = cls(my_maze, fixed_portals)
+        return s
+
+    def _getPositionsToPortals(self) -> dict[Pos_T, PortalConnection_T]:
+        """Use the portal labels to positions map to get a positions to portals map"""
+        to_return = {}
+        for portal in self._labels_to_portals.values():
+            if portal.inside_pos:
+                assert(portal.inside_pos not in to_return)
+                to_return[portal.inside_pos] = portal
+            if portal.outside_pos:
+                assert(portal.outside_pos not in to_return)
+                to_return[portal.outside_pos] = portal
+        return to_return
+    
+    @property
+    def start_pos(self) -> Pos_T:
+        """The position for label `AA`"""
+        p = self._labels_to_portals['AA']
+        if p.outside_pos is None:
+            return p.inside_pos
+        elif p.inside_pos is None:
+            return p.outside_pos
+        else:
+            raise RuntimeError(f"Bad format for position: {p}")
+    
+    @property
+    def end_pos(self) -> Pos_T:
+        """The position for label `ZZ`"""
+        p = self._labels_to_portals['ZZ']
+        if p.outside_pos is None:
+            return p.inside_pos
+        elif p.inside_pos is None:
+            return p.outside_pos
+        else:
+            raise RuntimeError(f"Bad format for position: {p}")
+
+    def tileAtPos(self, pos: Pos_T) -> TileType:
+        """Return the tile type at a position"""
+        return self._maze[pos[1]][pos[0]]
+
+    def findTimeBetweenPos(self, start_pos: Pos_T, end_pos: Pos_T, plutonian: bool = False) -> int:
+        """Return the time to move between `start_pos` and `end_pos`"""
+
+        print(f"Finding path from {start_pos} to {end_pos}")
+        if self.tileAtPos(start_pos) not in [TileType.PASSAGE, TileType.PORTAL]:
+            raise RuntimeError(f"start_pos ({start_pos}) is an invalid type: {self.tileAtPos(start_pos)}")
+        if self.tileAtPos(end_pos) not in [TileType.PASSAGE, TileType.PORTAL]:
+            raise RuntimeError(f"end_pos ({end_pos}) is an invalid type: {self.tileAtPos(end_pos)}")
+
+        # Since this is part 1, we are relatively bounded on depth
+        #   so just do a simple BFS
+        #   turns out this works on part2 too
+        best_times = {}
+        frontier = deque()
+        frontier.append((start_pos, 0, 0))
+
+        while len(frontier) > 0:
+            this_pos, this_dist, this_depth = frontier.popleft()
+
+            if this_depth < 0:
+                # this was not explicitly disallowed in the instructions
+                #   but apparently its a relevant condition
+                continue
+
+            if (this_pos, this_depth) in best_times:
+                continue
+            best_times[(this_pos, this_depth)] = this_dist
+
+            if this_pos == end_pos and this_depth == 0:
+                return this_dist
+            
+            # expand via portal
+            if self.tileAtPos(this_pos) == TileType.PORTAL:
+                portal = self._positions_to_portals[this_pos]
+
+                if portal.inside_pos == this_pos and portal.outside_pos:
+                    if plutonian:
+                        # print(f"Using {portal.label} to go {portal.inside_pos} to {portal.outside_pos} {this_depth}+1 depth")
+                        frontier.append((portal.outside_pos, this_dist + 1, this_depth + 1))
+                    else:
+                        frontier.append((portal.outside_pos, this_dist + 1, this_depth))
+                elif portal.outside_pos == this_pos and portal.inside_pos:
+                    if plutonian:
+                        # print(f"Using {portal.label} to go {portal.outside_pos} to {portal.inside_pos} {this_depth}-1 depth")
+                        frontier.append((portal.inside_pos, this_dist + 1, this_depth - 1))
+                    else:
+                        frontier.append((portal.inside_pos, this_dist + 1, this_depth))
+
+            # expand via cartesian neighbors
+            this_x, this_y = this_pos
+            for m_x, m_y in [(0,1),(1,0),(-1,0),(0,-1)]:
+                new_x = this_x + m_x
+                new_y = this_y + m_y
+
+                if self.tileAtPos((new_x, new_y)) in [TileType.PASSAGE, TileType.PORTAL]:
+                    frontier.append(((new_x, new_y), this_dist + 1, this_depth))
+
+        raise RuntimeError("There was no path between start_pos and end_pos")
+
+    def print(self) -> None:
+        """Print the maze to the console"""
+        for row in self._maze:
+            print("".join(map(lambda x: x.render(), row)))
+
+
 def y2019d20(inputPath = None):
     if(inputPath == None):
         inputPath = "Input2019/d20.txt"
@@ -34,171 +298,21 @@ def y2019d20(inputPath = None):
 
     with open(inputPath) as f:
         for line in f:
-            # horrifically slow
-            while line[-1] in ['\r', '\n']:
-                line = line[:-1]
             lineList.append(line)
     
+    maze = DonutMaze.fromLines(lineList)
 
-    # assert that the maze "starts at 2,2"
-    assert(lineList[0][0] == " ")
-    assert(lineList[1][0] == " ")
-    assert(lineList[0][1] == " ")
-    assert(lineList[1][1] == " ")
-    assert(lineList[0][2] == " ")
-    assert(lineList[1][2] == " ")
-    assert(lineList[2][0] == " ")
-    assert(lineList[2][1] == " ")
-    assert(lineList[2][2] == "#")
+    Part_1_Answer = maze.findTimeBetweenPos(
+        start_pos=maze.start_pos,
+        end_pos=maze.end_pos,
+    )
+    Part_2_Answer = maze.findTimeBetweenPos(
+        start_pos=maze.start_pos,
+        end_pos=maze.end_pos,
+        plutonian=True,
+    )
 
-    my_maze = []
-    portal_candidates = {}
-
-    # read in the walls and paths
-    for y_value, row in enumerate(lineList):
-        maze_row = []
-        for x_value, char in enumerate(row):
-            if char == ".":
-                maze_row.append(TileType.PASSAGE)
-            elif char == "#":
-                maze_row.append(TileType.WALL)
-            elif char == " ":
-                maze_row.append(TileType.WHITESPACE)
-            elif ((ord(char) >= ord('A')) and (ord(char) <= ord('Z'))):
-                portal_candidates[(x_value, y_value)] = char
-                maze_row.append(TileType.WHITESPACE)
-            else:
-                raise RuntimeError(f"Unrecognized character: {char}")
-        my_maze.append(maze_row)
-
-    def getPassagePosition(x_1,y_1,x_2, y_2) -> tuple[int, int]:
-        """get the passage position from the two letter positions"""
-        if y_1 == y_2:
-            # horizontal case
-            candidate_x_1 = min(x_1, x_2) - 1
-            candidate_x_2 = max(x_1, x_2) + 1 # candidate_1 + 2?
-            candidate_y_1 = y_1
-            candidate_y_2 = y_1
-        else:
-            # vertical case
-            candidate_x_1 = x_1
-            candidate_x_2 = x_1
-            candidate_y_1 = min(y_1, y_2) - 1
-            candidate_y_2 = max(y_1, y_2) + 1
-        
-        for x,y in [(candidate_x_1, candidate_y_1), (candidate_x_2, candidate_y_2)]:
-            try:
-                if x < 0 or y < 0:
-                    continue
-                # print(f"Checking {x},{y} -> ", my_maze[y][x])
-                if my_maze[y][x] == TileType.PASSAGE:
-                    return (x,y)
-            except IndexError:
-                pass
-        raise RuntimeError("No neighboring position was a passage")
-
-    # now match the portals
-    matched_portals = defaultdict(list)
-
-    while portal_candidates:
-        (this_x, this_y), this_letter = next(iter(portal_candidates.items()))
-
-        num_matches = 0
-
-        for other_x, other_y in [(this_x + 1, this_y), (this_x - 1, this_y), (this_x, this_y + 1), (this_x, this_y - 1)]:
-            try:
-               other_letter = portal_candidates[(other_x, other_y)]
-            except KeyError:
-                continue
-            
-            if other_x > this_x:
-                portal_name = f"{this_letter}{other_letter}"
-            elif other_x < this_x:
-                portal_name = f"{other_letter}{this_letter}"
-            elif other_y > this_y:
-                portal_name = f"{this_letter}{other_letter}"
-            else:
-                portal_name = f"{other_letter}{this_letter}"
-            # print("NAME: {} at {},{}".format(portal_name, this_x, this_y))
-            portal_pos = getPassagePosition(this_x, this_y, other_x, other_y)
-            matched_portals[portal_name].append(portal_pos)
-            num_matches += 1
-            my_maze[portal_pos[1]][portal_pos[0]] = TileType.PORTAL
-            portal_candidates.pop((this_x, this_y))
-            portal_candidates.pop((other_x, other_y))
-        if num_matches == 0:
-            raise RuntimeError(f"This {(this_x, this_y)} does not have another candidate nearby")
-        if num_matches > 1:
-            raise RuntimeError(f"This {(this_x, this_y)} does not multiple candidate nearby")
-
-    # check that all the portals have exactly two candidates in their named pair
-    for k,v in matched_portals.items():
-        if k in ['ZZ', 'AA']:
-            assert(len(v) == 1)
-            continue
-        if len(v) != 2:
-            print(f"Key {k} got only portal {v} ")
-        assert(len(v) == 2)
-
-    # # And that concludes the reading in part
-    # # display helper function
-    # for row in my_maze:
-    #     print("".join(map(lambda x: x.render(), row)))
-    # # ===================
-
-    # TODO - need to precompute components
-
-    # build reverse map
-    portal_to_label = {}
-    for label, positions in matched_portals.items():
-        for p in positions:
-            portal_to_label[p] = label
-
-    # now for the traversals, 
-    #   simple BFS
-
-    start_pos = matched_portals['AA'][0]
-    goal_pos = matched_portals['ZZ'][0]
-
-    best_times = {}
-
-    frontier = deque()
-    frontier.append((start_pos, 0))
-
-    while len(frontier) > 0:
-        this_pos, this_depth = frontier.popleft()
-
-
-        if this_pos in best_times:
-            continue
-        best_times[this_pos] = this_depth
-
-        if this_pos == goal_pos:
-            break
-
-        # print(f"Expanding {this_pos} from depth {this_depth}")
-        # check portal expansion
-        try:
-            portal_label = portal_to_label[this_pos]
-            # print(f"  This is a portal {portal_label} to ", end ="")
-            for n in matched_portals[portal_label]:
-                if n == this_pos:
-                    continue
-                frontier.append((n, this_depth + 1))
-                print(f" {n} ", end ="")
-            print("")
-        except KeyError:
-            pass
-
-        # cartesian expansion
-        this_x, this_y = this_pos
-        for m_x, m_y in [(0,1),(1,0),(-1,0),(0,-1)]:
-            new_x = this_x + m_x
-            new_y = this_y + m_y
-
-            if my_maze[new_y][new_x] in [TileType.PASSAGE, TileType.PORTAL]:
-                frontier.append(((new_x, new_y), this_depth + 1))
-
-    Part_1_Answer = best_times[goal_pos]
+    print(f"Part 2 guess: {Part_2_Answer}")
+    assert(Part_2_Answer > 3642)
 
     return (Part_1_Answer, Part_2_Answer)
--- Solutions2019/y2019d22.py
+++ Solutions2019/y2019d22.py
@@ -1,5 +1,108 @@
 # from AOC_Lib.name import *
 
+import itertools
+from typing import Iterable
+
+Card_T = int
+Deck_T = Iterable[Card_T]
+
+
+def dealIntoNewStack(deck: Deck_T) -> Deck_T:
+    """Deal the deck into a new stack and then use this as the new deck"""
+    l = list(deck)
+    return reversed(l)
+
+
+def _cutNegN(deck:Deck_T, n:int) -> Deck_T:
+    """Handler for when n < 0"""
+    assert(n < 0)
+    l = list(deck)
+    return itertools.chain(l[n:], l[:n])
+
+
+def cutN(deck: Deck_T, n: int) -> Deck_T:
+    """Preform the cut operation"""
+    if n == 0:
+        return deck
+    if n < 0:
+        return _cutNegN(deck, n)
+    
+    l = list(itertools.islice(deck, n))
+    return itertools.chain(deck, l)
+
+
+def dealWithIncrementN(deck: Deck_T, n: int) -> Deck_T:
+    """Deal with the increment n"""
+    old = list(zip(deck, itertools.count(0, n)))
+    l = len(old)
+    assert(l != 0)
+    old.sort(key=(lambda x: x[1]%l))
+    return map(lambda x: x[0], old)
+
+
+def shuffle(deck: Deck_T, deals: list[str]) -> Deck_T:
+    """Apply a series of deals onto deck"""
+    new_deck = deck
+    for d in deals:
+        s = d.split()
+
+        if s[0] == "cut":
+            new_deck = cutN(new_deck, int(s[1]))
+        elif s[0] == "deal":
+            if s[-1] == "stack":
+                new_deck = dealIntoNewStack(new_deck)
+            else:
+                new_deck = dealWithIncrementN(new_deck, int(s[-1]))
+        else:
+            raise ValueError(d)
+    return new_deck
+
+
+def shuffleFromFactoryOrder(num_cards: int, deals: list[str]) -> Deck_T:
+    """Do all the deals from a factory order deck of n cards"""
+    return shuffle(iter(range(num_cards)), deals)
+
+
+def y2019d22_tests() -> bool:
+    """Run tests and return `True` iff all are passing"""
+
+    r = shuffleFromFactoryOrder(10, [
+        "deal with increment 7",
+        "deal into new stack",
+        "deal into new stack",
+    ])
+    assert(list(r) == [0,3,6,9,2,5,8,1,4,7])
+
+    r = shuffleFromFactoryOrder(10, [
+        "cut 6",
+        "deal with increment 7",
+        "deal into new stack",
+    ])
+    assert(list(r) == [3,0,7,4,1,8,5,2,9,6])
+
+    r = shuffleFromFactoryOrder(10, [
+        "deal with increment 7",
+        "deal with increment 9",
+        "cut -2",
+    ])
+    assert(list(r) == [6,3,0,7,4,1,8,5,2,9])
+
+    r = shuffleFromFactoryOrder(10, [
+        "deal into new stack",
+        "cut -2",
+        "deal with increment 7",
+        "cut 8",
+        "cut -4",
+        "deal with increment 7",
+        "cut 3",
+        "deal with increment 9",
+        "deal with increment 3",
+        "cut -1",
+    ])
+    assert(list(r) == [9,2,5,8,1,4,7,0,3,6])
+    return True
+
+
 def y2019d22(inputPath = None):
     if(inputPath == None):
         inputPath = "Input2019/d22.txt"
@@ -13,11 +116,23 @@ def y2019d22(inputPath = None):
         for line in f:
             line = line.strip()
             lineList.append(line)
-    
-    # for single line inputs
-    for c in lineList[-1]:
-        pass
 
-    # for multi line inputs
+    assert(y2019d22_tests())
+
+    deck = shuffleFromFactoryOrder(10007, lineList)
+
+    for i, v in enumerate(deck):
+        if v == 2019:
+            Part_1_Answer = i
+            break
+
+    # Not sure how to continue
+    #   but fairly sure its Chinese Remainder Theorem:
+    #       Each step will be cyclic in some period
+    #       so the whole thing will be cyclic in some period
+    #       so we can get it to just a remainder
+    #   then rewrite the deals to take an index and size and say where
+    #       the next index is 
+    # TODO
 
     return (Part_1_Answer, Part_2_Answer)
