6b44ece5488d7bc29fba6ff95a62cb302091cee6
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2022-12-03 23:16:29 -0600

    y2022d4 refactored

--- Solutions2022/y2022d4.py
+++ Solutions2022/y2022d4.py
@@ -1,68 +1,65 @@
 
-
+from dataclasses import dataclass, field
 from typing import Optional
 
 
-from AOC_Lib.Geometry.LineSegment import Segment2_DiscreteAA
+from AOC_Lib.Geometry.LineSegment import Segment2_DiscreteAA, DegenerateMultipoint
 from AOC_Lib.Geometry.Point import DiscretePoint2
 from AOC_Lib.SolutionBase import SolutionBase, Answer_T
 
 
 
-class Elf:
+@dataclass
+class ElfRange:
 
+    low: int
+    high: int
 
-    def __init__(self, range: str) -> None:
-        a,_,b = range.partition('-')
-        self.low = min(int(a), int(b))
-        self.high = max(int(a), int(b))
+    _segment: Segment2_DiscreteAA = field(default=None, init=False)
 
-    def contains(self, other: 'Elf') -> bool:
+    def __post_init__(self):
+        self._segment = Segment2_DiscreteAA(
+            DiscretePoint2(self.low, 0),
+            DiscretePoint2(self.high, 0),
+        )
+
+    @classmethod
+    def from_str(cls, s: str) -> 'ElfRange':
+        """From the #-# format"""
+        a,_,b = s.partition('-')
+        l = min(int(a), int(b))
+        u = max(int(a), int(b))
+        return cls(low=l, high=u)
+
+    def contains(self, other: 'ElfRange') -> bool:
         return self.low <= other.low and self.high >= other.high
 
-    def overlaps(self, other: 'Elf') -> bool:
-        if self.low in range(other.low, other.high+1):
+    def overlaps(self, other: 'ElfRange') -> bool:
+        try:
+            return (self._segment.get_intersection(other._segment) is not None)
+        except DegenerateMultipoint:
             return True
-        if self.high in range(other.low, other.high+1):
-            return True
-        if other.low in range(self.low, self.high+1):
-            return True
-        if other.high in range(self.low, self.high+1):
-            return True
-        return False
+
 
 class Solution_2022_04(SolutionBase):
     """https://adventofcode.com/2022/day/4"""
 
     def __post_init__(self):
         """Runs Once After `__init__`"""
+
+        p1_counter = 0
+        p2_counter = 0
         
-        for line in self.input_str_list(include_empty_lines=False):
-            pass
-
-    def _part_1_hook(self) -> Optional[Answer_T]:
-        """Called once and return value is taken as `part_1_answer`"""
-
-        counter = 0
-
-        counter_2 = 0
-
         for line in self.input_str_list(include_empty_lines=False):
             r1, _, r2 = line.partition(',')
 
-            elf1 = Elf(r1)
-            elf2 = Elf(r2)
+            elf1 = ElfRange.from_str(r1)
+            elf2 = ElfRange.from_str(r2)
 
             if elf1.contains(elf2) or elf2.contains(elf1):
-                counter += 1
-
+                p1_counter += 1
             if elf1.overlaps(elf2):
-                counter_2 += 1
+                p2_counter += 1
+        self._part_1_answer = p1_counter
+        self._part_2_answer = p2_counter
 
-        self._part_2_answer = counter_2
-            
-        return counter
-
-
-    def _part_2_hook(self) -> Optional[Answer_T]:
-        """Called once and return value is taken as `part_2_answer`"""
